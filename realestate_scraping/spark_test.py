adad4a903d32da9ab5856a96abfc98dda151fba9
StephanKnox <stanislav.i.kulchitskiy@gmail.com>
2023-04-24 11:10:30 +0200

    More PySpark and Delta testing

--- realestate_scraping/spark_test.py
+++ realestate_scraping/spark_test.py
@@ -1,11 +1,11 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, StringType, IntegerType, DoubleType
 import pyspark.sql.functions as F
-#from delta import *
+from delta.tables import DeltaTable
 from delta.pip_utils import configure_spark_with_delta_pip
 
 
-# Initialize a Spark session
+## Initialize a Spark session
 spark = (
     SparkSession.builder \
     #builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
@@ -31,23 +31,66 @@ SCHEMA = StructType(
 )
 
 
-df_acidentes = (
+#df_acidentes = (
+#    spark
+#    .read.format("csv")
+#    .option("delimiter", ";")
+#    .option("header", "true")
+#    .option("encoding", "ISO-8859-1")
+#    .schema(SCHEMA)
+#   .load("realestate-scraping/realestate_scraping/datatran2022.csv")
+#)
+
+#df_acidentes.show(5)
+
+
+
+## Creating a Delta table from a PySpark dataframe
+#df_acidentes\
+#    .write.format("delta")\
+#    .mode("overwrite")\
+#    .save("realestate-scraping/realestate_scraping/delta")
+
+
+# Reading from a Delta table into a PySpark dataframe
+path_to_delta = 'file:///Users/ctac/Desktop/Data Engineer /Projects/realestate_scraping_project/realestate-scraping/realestate_scraping/delta'
+df_acidentes_delta = (
     spark
-    .read.format("csv")
-    .option("delimiter", ";")
-    .option("header", "true")
-    .option("encoding", "ISO-8859-1")
-    .schema(SCHEMA)
-    .load("realestate-scraping/realestate_scraping/datatran2022.csv")
+    .read.format("delta")
+    .load(path_to_delta)
 )
 
-df_acidentes.show(5)
+## Reading from a Delta table using SQL-like interface
+spark.sql(f"SELECT * FROM delta.`{path_to_delta}` WHERE dia_semana = 'sábado'").show()
+
+#df_acidentes_delta.select(["id", "data_inversa", "dia_semana", "horario", "uf"]).show(3)
+
+# Print count of number of rows in a Delta table
+#print(df_acidentes_delta.count())
+
+#spark.sql("select * from delta.`hdfs://192.168.2.131:9000/Delta_Table/test001`").show()
+
+## READING THE 2021 DATA
+#df_acidentes_2021 = (
+#    spark
+#    .read.format("csv")
+#    .option("delimiter", ";")
+#    .option("header", "true")
+#    .schema(SCHEMA)
+#    .load("file:///Users/ctac/Desktop/Data Engineer /Projects/realestate_scraping_project/realestate-scraping/realestate_scraping/datatran2021.csv")
+#)
+
+## Append data to a Delta table
+#df_acidentes_2021\
+#    .write.format("delta")\
+#    .mode("append")\
+#    .save(path_to_delta)
+
+#print(df_acidentes_delta.count())
 
 
+## Check Delta table history and versions
+#delta_table = DeltaTable.forPath(spark, path_to_delta)
+#delta_table.history().show()
 
-#spark.range(10).write.format("delta").option("path", "/realestate-scraping/realestate_scraping/").saveAsTable("delta")
-#df_acidentes.write.format("delta").mode("overwrite").save("realestate-scraping/realestate_scraping/people10m")
-df_acidentes\
-    .write.format("delta")\
-    .mode("overwrite")\
-    .save("realestate-scraping/realestate_scraping/delta")
+#delta_table.history().select("version", "timestamp", "operation", "operationParameters").show(10, False)
